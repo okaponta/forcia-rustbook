ebc0cc6fbaac112102767d34b864266f3667b455
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-28 00:20:09 +0900

    rewrite using structopt.

--- chapter4/samplecli/src/main.rs
+++ chapter4/samplecli/src/main.rs
@@ -1,30 +1,28 @@
-use clap::{App, Arg};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "My RPN Program",
+    version = "1.0.0",
+    author = "okaponta",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opt {
+    // Sets the level of verbosity
+    #[structopt(short, long)]
+    verbose: bool,
+
+    // Formulas written in RPN
+    #[structopt(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("My RPN Program")
-        .version("1.0.0")
-        .author("okaponta")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::with_name("formula_file")
-                .help("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .help("Sets the level of verbosity")
-                .short("v")
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
+    let opts = Opt::from_args();
 
-    match matches.value_of("formula_file") {
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified"),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
