bb5e9df13976f8e7e9bccd44f4334fa7d035d783
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-06 21:28:53 +0900

    update ui_demo

--- chapter7/ui_demo/src/main.rs
+++ chapter7/ui_demo/src/main.rs
@@ -1,10 +1,23 @@
-use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+use iced::{
+    button, executor, Align, Application, Button, Clipboard, Column, Command, Element, Font,
+    HorizontalAlignment, Length, Row, Settings, Text,
+};
+
+const FONT: Font = Font::External {
+    name: "PixelMplus12-Regular",
+    bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
+};
 
 fn main() -> iced::Result {
-    GUI::run(Settings::default())
+    let mut settings = Settings::default();
+    settings.window.size = (400u32, 120u32);
+    GUI::run(settings)
 }
 
-struct GUI;
+struct GUI {
+    start_stop_button_state: button::State,
+    reset_button_state: button::State,
+}
 
 impl Application for GUI {
     type Executor = executor::Default;
@@ -12,11 +25,17 @@ impl Application for GUI {
     type Flags = ();
 
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
-        (GUI, Command::none())
+        (
+            GUI {
+                start_stop_button_state: button::State::new(),
+                reset_button_state: button::State::new(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
-        String::from("DEMO")
+        String::from("STOPWATCH")
     }
 
     fn update(
@@ -28,6 +47,35 @@ impl Application for GUI {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, World!").into()
+        let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
+        let start_stop_button = Button::new(
+            &mut self.start_stop_button_state,
+            Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Reset")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        Column::new()
+            .push(tick_text)
+            .push(
+                Row::new()
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
+            )
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Align::Center)
+            .into()
     }
 }
